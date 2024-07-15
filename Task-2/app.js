3e6dda422c6b81cb59d03bd0d1505715fe063fd2
Mohammad-Ayan <ayan2015786@gmail.com>
2024-07-15 12:15:42 +0530

    Operators

---
+++ Task-2/app.js
@@ -0,0 +1,66 @@
+//-Activity: Arithmetic Operations
+//Task 1: WAP to add 2 no's 
+
+// let a = 3;
+// let b = 34;
+// const sum = a + b;
+// console.log(sum);
+
+//Task 2: WAP to substract 2 no's 
+
+// let a = 3;
+// let b = 34;
+// const sub = a - b;
+// console.log(sub);
+
+//Task 3: WAP to multiply 2 no's 
+
+// let a = 3;
+// let b = 34;
+// const mul = a * b;
+// console.log(mul);
+
+//Task 4: WAP to divide 2 no's 
+
+// let a = 68;
+// let b = 34;
+// const div = a / b;
+// console.log(div);
+
+//Task 5: WAP to divide 2 no's and find the remainder
+
+// let a = 70;
+// let b = 34;
+// const modulo = a % b;
+// console.log(modulo);
+
+//Activity: Assignment Operators
+//Task 6: Using += operator to add a number to a variable
+
+// let a = 70;
+// a += 35;  //variable = variable + value
+// console.log(a);
+
+//Task 7: Using -= operator to add a number to a variable
+
+// let a = 70;
+// a -= 45;  //variable = variable + value
+// console.log(a);
+
+//Task 8: WAP to compare 2 no's using > & < 
+
+// let a = 70;
+// let b = 34;
+// const boolean1 = a < b;
+// const boolean2 = a > b;
+// console.log(boolean1);
+// console.log(boolean2);
+
+//Task 9: WAP to compare 2 no's using >= & <= 
+
+// let a = 70;
+// let b = 34;
+// const boolean1 = a <= b;
+// const boolean2 = a >= b;
+// console.log(boolean1);
+// console.log(boolean2);
