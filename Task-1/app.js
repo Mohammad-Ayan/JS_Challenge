094f9cb6b27862af0e020b818eaa48caf22c3c3d
Mohammad-Ayan <ayan2015786@gmail.com>
2024-07-19 23:47:53 +0530

    Variables and DataTypes

--- Task-1/app.js
+++ Task-1/app.js
@@ -1,49 +1,49 @@
 // -Activity-1: Variable Declaration
 //Task 1:
 
-// var number = 8;
-// console.log(number);
+var number = 8;
+console.log(number);
 
 //Task 2:
 
-// let string = "ayan";
-// console.log(string);
+let string = "ayan";
+console.log(string);
 
 //-Activity-2: Constant Declaration
 //Task 3:
 
-// const boolean = true;
-// console.log(boolean);
+const boolean = true;
+console.log(boolean);
 
 //-Activity-3:  Data Types
 //Task 4:
 
-// let a = 3;
-// let b = "ayan";
-// let c = false;
-// let arr = [2, 4, 6, 9];
-// const obj = {
-//     a: "task",
-//     b: "js"
-// }
+let a = 3;
+let b = "ayan";
+let c = false;
+let arr = [2, 4, 6, 9];
+const obj = {
+    a: "task",
+    b: "js"
+}
 
 //console.log(typeof a, typeof b, typeof c, typeof arr, typeof obj);
 
 //-Activity-4: Reassigning Variables
 // Task 5:
 
-// let a = 10;
-// console.log(a);
-// a = a+5
-// console.log(b);
+let a = 10;
+console.log(a);
+a = a+5
+console.log(b);
 
 //-Activity-5: Understanding Const
 // Task 6:
 
-// const number = 10;
-// console.log(number);
-// number = number + 5;// TypeError: Assignment to constant variable.
-// console.log(number);
+const number = 10;
+console.log(number);
+number = number + 5;// TypeError: Assignment to constant variable.
+console.log(number);
 
 
 
