df101786c3de51d96aa2ec920799a97859a05f7c
Mohammad-Ayan <ayan2015786@gmail.com>
2024-08-01 20:14:14 +0530

    .

---
+++ Task-5/app.js
@@ -0,0 +1,17 @@
+// Day-5: Function
+
+//Activity 1: Function Declaration 
+//Task1: WAP to check if a number  is even or odd and log the result to the console 
+
+// function isEvenorOdd(number) {
+//     if (number % 2 === 0) {
+//         console.log(number + " is even");
+//     } else {
+//         console.log(number + " is odd");
+//     }
+// }
+
+// isEvenorOdd(7);
+// isEvenorOdd(2);
+
+//Task2: WAP to check if a number  is even or odd and log the result to the console
