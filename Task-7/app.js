891c551125d586d2339c0678ae3aba28ced414e8
Mohammad-Ayan <ayan2015786@gmail.com>
2024-08-07 21:50:03 +0530

    .

--- Task-7/app.js
+++ Task-7/app.js
@@ -58,5 +58,39 @@ Book3.updateYear(2024);
 console.log(Book3);
 
 //Activity 3: Nested Objects
-//Task 4: Add a method to the book object that takes a paremeter (year) and updates the book's year properly,
-// then log the updated object.
+//Task 5: Create a nested object representing a library with properties like name and books(an array of book objects),
+//and log the library object to the console.
+
+const library = {
+    name: "36 Creasent Library",
+    books: [
+        {
+            title: "yxyz",
+            author: "Harper Lee",
+            year: 1960
+
+        },
+        {
+            title: "pqrz",
+            author: "Harper Lee",
+            year: 1960
+        },
+        {
+            title: "abcz",
+            author: "Harper Lee",
+            year: 1960
+        }
+    ]
+};
+
+console.log(library);
+
+//Task 6: Access and log the name of the library and the titles of all the books in the library.
+
+library.books.forEach(book => {
+    console.log(library.name + " " + book.title);
+});
+
+//Activity 4: The this Keyword
+//Task 7: Add a method to the book object that uses the this keyword to return a string with the book's title 
+// and year and log the result of calling this method.
