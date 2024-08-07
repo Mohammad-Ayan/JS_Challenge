9af646896f6e813b17b37c7e6ce12fb989121249
Mohammad-Ayan <ayan2015786@gmail.com>
2024-08-08 00:39:50 +0530

    .

---
+++ Task-8/app.js
@@ -0,0 +1,5 @@
+// Day-8 ES6+ Features
+
+//Activity 1: Template Literals
+//Task 1: Use Tempelate literals to create a string that includes variables for a person's name and age,
+// and log the string to the console
